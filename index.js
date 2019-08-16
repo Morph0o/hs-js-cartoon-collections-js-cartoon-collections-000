8d4a8dd084ee76cba73cc5c4a2b645c0ec512f6c
collaborative-bug-2010 <detrodoniv@yahoo.com>
2019-08-16 19:52:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function dwarfRollCall(dwarves) {
-  var num = []
-  for(i = 0,i<dwarves.length,i++){
-  number.push(`)}
+  var num = {};
+  for(let i = 0;i<dwarves.length;i++){
+  num = `${i+1}. ${dwarves[i]}`
+    }
+  return num
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
+  var upper = [];
+  for(let i = 0;i <planeteerCalls;i++){
+  upper.push(`${planeteerCalls[i].toUpperCase}!`);
+    }
+    return upper;
 }
 
 function longPlaneteerCalls(words) {
+  longwords = ["earth","heart","water"]
 }
 
 function findTheCheese (foods) {
